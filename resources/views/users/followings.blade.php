0f28da9600c9e05aa992cea8e538d0ad0a0b1d02
gatapon <gatapon1988@gmail.com>
2018-10-25 05:11:49 +0000

    Procfile

--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 
     <div class="row">
@@ -16,21 +17,12 @@
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
-                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine<span class="badge">{{ $count_microposts }}</span></a></li>
+                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show',['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
-            </ul>
-            @if (Auth::id() == $user->id)
-                {!! Form::open(['route' => 'microposts.store']) !!}
-                    <div class="form-group">
-                        {!! Form::textarea('content', old('content'),['class' => 'form-control', 'row' => '2']) !!}
-                        {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
-                    </div>
-                {!! Form::close() !!}
-            @endif
-            @if (count($microposts) > 0)
-                @include('microposts.microposts', ['microposts' => $microposts])
-            @endif
+            </ul> 
+            <@include('users.users',['users' => $users])
         </div>
     </div>
+
 @endsection
